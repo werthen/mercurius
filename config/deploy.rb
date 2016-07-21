180963a90c20ce20edbc77d4d7529b77c724494e
Lorin Werthen <werthenlorin@gmail.com>
2016-07-21 19:18:34 +0200

    format airbrussh

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,9 +14,6 @@ set :deploy_to, '/var/www/mercurius'
 # Default value for :scm is :git
 set :scm, :git
 
-# Default value for :format is :pretty
-set :format, :pretty
-
 set :user, 'mercurius'
 
 set :use_sudo, false
